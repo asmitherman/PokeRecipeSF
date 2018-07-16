c9a52cc56b05a7513af002ddacc84b4bc27af96a
Austin Smitherman <asmitherman@mail.csuchico.edu>
2018-07-16 12:42:37 -0400

    new pipeline

--- destroy1531756803.sh
+++
@@ -1,5 +0,0 @@
-heroku pipelines:destroy pipeline1531756803
-heroku apps:destroy -a dev1531756803 -c dev1531756803
-heroku apps:destroy -a staging1531756803 -c staging1531756803
-heroku apps:destroy -a prod1531756803 -c prod1531756803
-rm -- "destroy1531756803.sh"
